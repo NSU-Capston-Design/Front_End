1ae9656853f5065ff41b36781847635d2eb2ed17
canola0701 <yrin2001@gmail.com>
2023-11-24 12:52:53 +0900

    게시글모달

---
+++ capstone_fe/src/component/Posting.js
@@ -0,0 +1,30 @@
+import React from "react";
+import '../css/Posting.css';
+
+const Posting = ({ onClose }) => {
+    return (
+        <form>
+            <div className="Posting">
+                <div className="Posting_header">
+                    <div className="Posting_title">
+                        <textarea className="Posting_titleInput">
+                            글 제목
+                        </textarea>
+                        <button type="button" className="Posting_closeBtn" onClick={onClose}>
+                            닫기
+                        </button>
+                    </div>
+                </div>
+                <textarea name="Posting_content">
+                    {/* 게시글 내용 작성 칸 */}
+                </textarea>
+                <button className="posting_btn">
+                    {/* 게시 버튼 */}
+                    게시
+                </button>
+            </div>
+        </form>
+    );
+};
+
+export default Posting;
